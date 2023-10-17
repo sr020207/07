a562dc20b32534a495baa946c7ba3cb7e94db2f1
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-10-17 15:53:53 +0900

    001

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int flag = 1;
+	while(flag != 0)
+	{
+		int y;
+		y = 3;
+		flag = 0;
+	 } 
+	 // y=4;
+	 // 블록 내부에서 정의한 변수를 밖에서 사용 
+	return 0;
+}

f42081c4f551e72bfcf23fe3cf778a1889db34cb
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-10-19 16:11:23 +0900

    005

--- main.c
+++ main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-void f(void);
 
-int i; 
+void sub(void);
+
+
 int main(int argc, char *argv[])
 {
-   for ( i=0; i<5; i++)
-   {
-      f();
-   }
-   
-   system("PAUSE");
+   int i;
+   for ( i=0; i<3; i++)
+   sub();
    return 0;
 }
 
-void f(void)
+void sub(void)
 {
-   for ( i =0; i<10; i++)
-   printf("#");
+	
+   int auto_count = 0;
+   static int static_count = 0;
+   printf("auto_count=%d\n", auto_count);
+   printf("static_count=%d\n", static_count);
+   auto_count++;
+   static_count++;
+   
+   
 }

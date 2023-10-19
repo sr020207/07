8bd9728f393a5580acc1270192abfcf413b6eda2
sr020207 <144992835+sr020207@users.noreply.github.com>
2023-10-19 16:03:36 +0900

    004

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+void f(void);
 
-int inc (int counter);
+int i; 
 int main(int argc, char *argv[])
 {
-
-	
-	int i = 10;
-	printf("함수 호출전 i=%d\n", i);
-	i = inc(i); //함수 안 결과가 이 변수에 들어간다 
-	printf("함수 호출후 i=%d\n", i);
-	return 0;
+   for ( i=0; i<5; i++)
+   {
+      f();
+   }
+   
+   system("PAUSE");
+   return 0;
 }
 
-int inc(int counter)
+void f(void)
 {
-	counter++;
-	return counter;
+   for ( i =0; i<10; i++)
+   printf("#");
 }
-
